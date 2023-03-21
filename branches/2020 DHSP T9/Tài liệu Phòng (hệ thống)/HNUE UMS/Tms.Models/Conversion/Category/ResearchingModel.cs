a3aa2dc728e32b0e889b0f47a8dabd66fb518c4a
DuowngTora <nvduong216@gmail.com>
2023-03-21 21:26:49 +0700

    Add Tài Liệu Phòng (Hệ thống): UMS và ID

---
+++ branches/2020 DHSP T9/Tài liệu Phòng (hệ thống)/HNUE UMS/Tms.Models/Conversion/Category/ResearchingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ums.Models.Conversion.Category
+{
+    public class ResearchingModel
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
